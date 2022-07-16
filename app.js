5ef726961eef90c3c29f9e9383a8b2e20d65bfc6
b-hayes <bradley.hayes@yourgiving.co>
2022-07-16 21:23:44 +1000

    profiles with styles for male or female

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ createApp({
             message: 'Vue JS is working!',
             firstName: 'Jim',
             lastName: 'Jones',
+            gender: 'male',
             email: 'jimmy@jones.com',
             picture: 'https://image.shutterstock.com/image-photo/portrait-excited-casual-man-standing-260nw-535171852.jpg'
         }
