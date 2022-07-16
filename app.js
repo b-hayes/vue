ef2b920f88a8da46d2f48438bd6e62617f58deba
b-hayes <bradley.hayes@yourgiving.co>
2022-07-16 21:12:20 +1000

    weird how the vuejs syntax changes a lot inside vbind

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Vue JS is working!'
+            message: 'Vue JS is working!',
+            firstName: 'Jim',
+            lastName: 'Jones',
+            email: 'jimmy@jones.com',
+            picture: 'https://image.shutterstock.com/image-photo/portrait-excited-casual-man-standing-260nw-535171852.jpg'
         }
     }
 }).mount('#app')
