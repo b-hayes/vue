010b32a593d88310411d97e1bdb8fa9d3980a088
b-hayes <bradley.hayes@yourgiving.co>
2022-07-23 18:14:10 +1000

    OMFG I just created the best button component ever!

--- vue-cli-created-project/src/App.vue
+++ vue-cli-created-project/src/App.vue
@@ -1,54 +1,55 @@
 <template>
-  <div class="container" style="background-color: grey">
+  <div class="container">
     <app-header/>
     <Button></Button>
-    <Button color="lightgrey">large Button</Button>
-    <Button color="magenta">large Button</Button>
-    <Button color="green">large Button</Button>
-    <Button color="orange">large Button</Button>
-    <Button color="blue">large Button</Button>
-    <Button color="red">large Button</Button>
-    <Button color="yellow">large Button</Button>
-    <Button color="lightgreen">large Button</Button>
-    <Button color="purple">large Button</Button>
-    <Button color="white">large Button</Button>
-    <Button color="black">large Button</Button>
-    <Button size="small">small Button</Button>
-    <Button size="small" color="lightgrey">small Button</Button>
-    <Button size="small" color="magenta">small Button</Button>
-    <Button size="small" color="green">small Button</Button>
-    <Button size="small" color="orange">small Button</Button>
-    <Button size="small" color="blue">small Button</Button>
-    <Button size="small" color="red">small Button</Button>
-    <Button size="small" color="yellow">small Button</Button>
-    <Button size="small" color="lightgreen">small Button</Button>
-    <Button size="small" color="purple">small Button</Button>
-    <Button size="small" color="white">small Button</Button>
-    <Button size="small" color="black">small Button</Button>
-    <Button size="medium">medium Button</Button>
-    <Button size="medium" color="lightgrey">medium Button</Button>
-    <Button size="medium" color="magenta">medium Button</Button>
-    <Button size="medium" color="green">medium Button</Button>
-    <Button size="medium" color="orange">medium Button</Button>
-    <Button size="medium" color="blue">medium Button</Button>
-    <Button size="medium" color="red">medium Button</Button>
-    <Button size="medium" color="yellow">medium Button</Button>
-    <Button size="medium" color="lightgreen">medium Button</Button>
-    <Button size="medium" color="purple">medium Button</Button>
-    <Button size="medium" color="white">medium Button</Button>
-    <Button size="medium" color="black">medium Button</Button>
-    <Button size="xx-large">xx-large Button</Button>
-    <Button size="xx-large" color="lightgrey">xx-large Button</Button>
-    <Button size="xx-large" color="magenta">xx-large Button</Button>
-    <Button size="xx-large" color="green">xx-large Button</Button>
-    <Button size="xx-large" color="orange">xx-large Button</Button>
-    <Button size="xx-large" color="blue">xx-large Button</Button>
-    <Button size="xx-large" color="red">xx-large Button</Button>
-    <Button size="xx-large" color="yellow">xx-large Button</Button>
-    <Button size="xx-large" color="lightgreen">xx-large Button</Button>
-    <Button size="xx-large" color="purple">xx-large Button</Button>
-    <Button size="xx-large" color="white">xx-large Button</Button>
-    <Button size="xx-large" color="black">xx-large Button</Button>
+    <Button color="grey" text="large Button"></Button>
+    <Button color="magenta" text="large Button"></Button>
+    <Button color="green" text="large Button"></Button>
+    <Button color="orange" text="large Button"></Button>
+    <Button color="blue" text="large Button"></Button>
+    <Button color="red" text="large Button"></Button>
+    <Button color="yellow" text="large Button"></Button>
+    <Button color="lightgreen" text="large Button"></Button>
+    <Button color="purple" text="large Button"></Button>
+    <Button color="white" text="large Button"></Button>
+    <Button color="black" text="large Button"></Button>
+    <Button size="small" text="small Button"></Button>
+    <Button size="small" color="grey" text="small Button"></Button>
+    <Button size="small" color="magenta" text="small Button"></Button>
+    <Button size="small" color="green" text="small Button"></Button>
+    <Button size="small" color="orange" text="small Button"></Button>
+    <Button size="small" color="blue" text="small Button"></Button>
+    <Button size="small" color="red" text="small Button"></Button>
+    <Button size="small" color="yellow" text="small Button"></Button>
+    <Button size="small" color="lightgreen" text="small Button"></Button>
+    <Button size="small" color="purple" text="small Button"></Button>
+    <Button size="small" color="white" text="small Button"></Button>
+    <Button size="small" color="black" text="small Button"></Button>
+    <Button size="medium" text="medium Button"></Button>
+    <Button size="medium" color="grey" text="medium Button"></Button>
+    <Button size="medium" color="magenta" text="medium Button"></Button>
+    <Button size="medium" color="green" text="medium Button"></Button>
+    <Button size="medium" color="orange" text="medium Button"></Button>
+    <Button size="medium" color="blue" text="medium Button"></Button>
+    <Button size="medium" color="red" text="medium Button"></Button>
+    <Button size="medium" color="yellow" text="medium Button"></Button>
+    <Button size="medium" color="lightgreen" text="medium Button"></Button>
+    <Button size="medium" color="purple" text="medium Button"></Button>
+    <Button size="medium" color="white" text="medium Button"></Button>
+    <Button size="medium" color="black" text="medium Button"></Button>
+    <Button size="xx-large" text="xx-large Button"></Button>
+    <Button size="xx-large" color="grey" text="xx-large Button"></Button>
+    <Button size="xx-large" color="magenta" text="xx-large Button"></Button>
+    <Button size="xx-large" color="green" text="xx-large Button"></Button>
+    <Button size="xx-large" color="orange" text="xx-large Button"></Button>
+    <Button size="xx-large" color="blue" text="xx-large Button"></Button>
+    <Button size="xx-large" color="red" text="xx-large Button"></Button>
+    <Button size="xx-large" color="yellow" text="xx-large Button"></Button>
+    <Button size="xx-large" color="lightgreen" text="xx-large Button"></Button>
+    <Button size="xx-large" color="purple" text="xx-large Button"></Button>
+    <Button size="xx-large" color="white" text="xx-large Button"></Button>
+    <Button size="xx-large" color="black" text="xx-large Button"></Button>
+    <Button><img alt="Vue logo" src="./assets/logo.png"></Button>
   </div>
 </template>
 
@@ -71,8 +72,10 @@ export default {
 </script>
 
 <style>
-#app {
+* {
   box-sizing: border-box;
+}
+#app {
   display: flex;
   justify-content: center;
 }
--- vue-cli-created-project/src/components/Button.vue
+++ vue-cli-created-project/src/components/Button.vue
@@ -2,10 +2,9 @@
   <!--  <button :style="`background-color: ${color}`"> NOTE: this works but so does the method below.
    see: https://vuejs.org/guide/essentials/class-and-style.html
    -->
-  <button :style="{backgroundColor: color, fontSize: size}">
-    <span  :style="{color: color}">
-      <slot>Click Me!</slot>
-    </span>
+  <button>
+    <slot></slot>
+    <label>{{ text }}</label>
   </button>
 </template>
 
@@ -14,9 +13,13 @@ export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Button",
   props: {
+    text: {
+      type: String,
+      default: 'Click Me!'
+    },
     color: {
       type: String,
-      default: 'grey'
+      default: 'lightgrey'
     },
     size: {
       type: String,
@@ -27,23 +30,43 @@ export default {
 </script>
 
 <style scoped>
-  button {
-    margin: 0.1em;
-    padding: 0.5em 1em;
-    border: 0.1em solid;
-    border-color: rgba(255,255,255, 0.5) rgba(0,0,0, 0.5) rgba(0,0,0, 0.5) rgba(255,255,255, 0.5);
-    border-radius: 0.3em;
-    /*box-shadow: 0.1em 0.1em 0.1em 0.1em rgba(0, 0, 0, 0.3);*/
-  }
-  button:hover {
-    box-shadow: inset 0 0 0 10em rgba(255, 255, 255, 0.1);
-  }
-  button:active {
-    box-shadow: inset 0em 0em 0em 10em rgba(0, 0, 0, 0.3);
-    border-color: rgba(0,0,0, 0.5) rgba(255,255,255, 0.5) rgba(255,255,255, 0.5) rgba(0,0,0, 0.5);
-  }
-  /* Span contains the button text */
-  span {
-    filter: grayscale(100%) contrast(100000%) invert(1);
-  }
+button {
+  --button-color: v-bind(color);
+  --button-size: v-bind(size);
+  font-size: var(--button-size);
+  color: var(--button-color);
+  background-color: var(--button-color);
+  margin: 0.1em;
+  padding: 0.5em 1em;
+  border: 0.1em solid;
+  border-color: rgba(255, 255, 255, 0.5) rgba(0, 0, 0, 0.5) rgba(0, 0, 0, 0.5) rgba(255, 255, 255, 0.5);
+  border-radius: 0.3em;
+  /*box-shadow: 0.1em 0.1em 0.1em 0.1em rgba(0, 0, 0, 0.3);*/
+  display: flex;
+  /*flex-direction: column;*/
+  align-items: center;
+}
+
+button:hover {
+  /*box-shadow: inset 0 0 0 10em rgba(255, 255, 255, 0.1);*/
+  filter: invert(0.1);
+}
+
+button:active {
+  box-shadow: inset 0em 0em 0em 10em rgba(0, 0, 0, 0.3);
+  border-color: rgba(0, 0, 0, 0.5) rgba(255, 255, 255, 0.5) rgba(255, 255, 255, 0.5) rgba(0, 0, 0, 0.5);
+}
+
+button:active label {
+  filter: invert(0.1);
+  text-shadow: 0.05em 0.05em black;
+}
+
+/* Span contains the button text */
+label {
+  /* This always works but generates black or white text only */
+  /*filter: grayscale(100%) contrast(100000%) invert(1);*/
+  /* this one retains some of the original colour but it doesnt look nice for all colours*/
+  filter: hue-rotate(160deg) brightness(80%) invert(1) saturate(100%) contrast(1000%);
+}
 </style>
