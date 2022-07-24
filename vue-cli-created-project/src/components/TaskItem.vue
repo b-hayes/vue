8a4682e17c6b8f53a7933945d2ed2d2656e4e91b
b-hayes <bradley.hayes@yourgiving.co>
2022-07-24 16:39:15 +1000

    Task components

---
+++ vue-cli-created-project/src/components/TaskItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="task-row">
+    <Button color="darkseagreen" text="">
+      <i class="fa fa-check"/>
+    </Button>
+    <input type="text" :value="task.text" disabled>
+    <Button text="" color="lightgrey">
+      <i class="fa fa-bars"/>
+    </Button>
+  </div>
+</template>
+
+<script>
+
+import Button from "@/components/Button";
+export default {
+  name: "TaskItem",
+  components: {Button},
+
+  props: {
+    task: Object
+  }
+}
+</script>
+
+<style scoped>
+input {
+  flex-grow: 1;
+}
+input:disabled {
+  border-color: rgba(0,0,0,0);
+}
+input:disabled:hover{
+  box-shadow: none;
+}
+button {
+  max-width: 2.8em;
+  background-color: unset;
+  border: none;
+}
+</style>
---
+++ vue-cli-created-project/src/components/TaskList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="task-list" v-for="task in tasks" :key="task.id">
+    <TaskItem :task="task"/>
+  </div>
+</template>
+
+<script>
+import TaskItem from "@/components/TaskItem";
+export default {
+  name: "TaskList",
+  components: {TaskItem},
+  props: {
+    tasks: Array
+  }
+}
+</script>
+
+<style scoped>
+  .task-list {
+
+  }
+</style>
